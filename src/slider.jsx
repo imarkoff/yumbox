2fe542ec6f324a075966a3c608844f7f4dbd6546
Vladyslav Hroshev <iv.markov02@gmail.com>
2024-05-18 14:04:57 +0300

    Swiper improvements

--- src/slider.jsx
+++ src/slider.jsx
@@ -1,32 +1,58 @@
+import { useRef } from 'react';
 import { Navigation , Pagination} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
 import sliderImg from './assets/img_slider.png'
 
 export default function Slider(){
+    const swiperRef = useRef(null);
+
+    const onNext = () => {
+        swiperRef.current.swiper.slideNext();
+    }
+
+    const onPrev = () => {
+        swiperRef.current.swiper.slidePrev();
+    }
+
     return(
-        <Swiper
-            modules={[Navigation, Pagination]}
-            navigation
-            spaceBetween={50}
-            slidesPerView={1}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-            
-        >
-            <SwiperSlide>
-                <img src={sliderImg} alt='slider image'></img>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={sliderImg} alt='slider image'></img>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={sliderImg} alt='slider image'></img>
-            </SwiperSlide>
-        </Swiper>
+        <>
+            <Swiper
+                ref={swiperRef}
+                modules={[Navigation, Pagination]}
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{
+                    clickable: true,
+                    el: '.swiper-pagination',
+                }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                    clickable: true,
+                }}
+                scrollbar={{ draggable: true }}
+            >
+                <SwiperSlide>
+                    <img src={sliderImg} alt='slider image'></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImg} alt='slider image'></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={sliderImg} alt='slider image'></img>
+                </SwiperSlide>
+                <div className="swiper-button-next"></div>
+                <div className="swiper-button-prev"></div>
+            </Swiper>
+            <div className="swiper-controls">
+                <div className="swiper-button-prev" onClick={onPrev}></div>
+                <div className="swiper-pagination"></div>
+                <div className="swiper-button-next" onClick={onNext}></div>
+            </div>
+        </>
     );
 }
